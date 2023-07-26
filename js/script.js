2452a964d581f9649c3bcdfc6a64671c8d3290df
Jim0305 <46538695+Jim0305@users.noreply.github.com>
2023-07-26 17:44:21 +0100

    Added movie details image backdrop

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ async function displayPopularMovies() {
                 ? `<img
               src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
               class="card-img-top"
-              alt="Movie Title"
+              alt="${movie.title}"
             />`
                 : `<img
               src="images/no-image.jpg"
@@ -47,7 +47,7 @@ async function displayPopularTVShows() {
                 ? `<img
               src="https://image.tmdb.org/t/p/w500${show.poster_path}"
               class="card-img-top"
-              alt="Movie Title"
+              alt="${show.name}"
             />`
                 : `<img
               src="images/no-image.jpg"
@@ -70,6 +70,8 @@ async function displayPopularTVShows() {
 async function displayMovieDetails() {
   const movieId = window.location.search.split('=')[1];
   const movie = await fetchAPIData(`movie/${movieId}`);
+  // Overlay for background image
+  displayBackgroundImage('movie', movie.backdrop_path);
   const div = document.createElement('div');
   div.innerHTML = `
   <div class="details-top">
@@ -79,7 +81,7 @@ async function displayMovieDetails() {
                 ? `<img
               src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
               class="card-img-top"
-              alt="Movie Title"
+              alt="${movie.title}"
             />`
                 : `<img
               src="images/no-image.jpg"
@@ -127,6 +129,28 @@ async function displayMovieDetails() {
             .join(', ')}</div>
         </div>`;
   document.querySelector('#movie-details').appendChild(div);
+}
+
+// Display Backdrop on details page
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
 }
 
 // Fetch data from TMDB API
